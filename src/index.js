93301adac33c29422b809be35d82cfbb2c4ba24f
Enrique Galindo <egalindo@protonmail.com>
2020-02-05 20:28:42 -0500

    added firebase

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import './conf/Firebase'
+import './Components/Main'
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
